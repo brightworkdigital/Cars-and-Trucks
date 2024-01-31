cde09bfdf4dfd5b8580b4971c07b285dd133604f
Dave <davedykes@davedykes.com>
2024-01-31 15:29:43 +0000

    Added alert dismissal scenario and associated step defs.

--- src/test/java/stepdefinitions/AlertStepDefs.java
+++ src/test/java/stepdefinitions/AlertStepDefs.java
@@ -4,13 +4,16 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AlertStepDefs {
 
@@ -28,6 +31,9 @@ public class AlertStepDefs {
     public void i_click_on_the_button(String buttonName) {
         List<WebElement> inputElements =  StaticUtils.webDriver.findElements(By.cssSelector("input"));
 
+//        List<WebElement> buttons = StaticUtils.webDriver.findElements(By.xpath(
+//                "//input[@type='button' ][@value='" + buttonName + "'] | //button[contains(text(), '" + buttonName + "')]"));
+
         boolean foundSendButton = false;
         for(WebElement element: inputElements) {
             String role = element.getAriaRole();
@@ -44,7 +50,23 @@ public class AlertStepDefs {
     }
     @Then("an alert window appears")
     public void an_alert_window_appears() {
+        Alert alert = StaticUtils.webDriver.switchTo().alert();
+        assertNotNull(alert, "There's not alter dialog");
+    }
+
+
+
+    @When("I dismiss the alert")
+    public void i_dismiss_the_alert()  {
+        Alert alert = StaticUtils.webDriver.switchTo().alert();
+        assertNotNull(alert, "There's not alter dialog");
+        alert.dismiss();
+    }
+    @Then("I am on the {string} page")
+    public void i_am_on_the_contact_page(String pageTitle) {
         // Write code here that turns the phrase above into concrete actions
+        String actualPageTitle = StaticUtils.webDriver.getTitle();
+        assertEquals(pageTitle, actualPageTitle);
     }
 
 }
--- src/test/resources/features/alert.feature
+++ src/test/resources/features/alert.feature
@@ -5,3 +5,9 @@ Feature: Test the contact form alert
     When I click on the "Send" button
     Then an alert window appears
 
+  Scenario: No name entered, clear alert
+    Given I am on the page "https://www.thetestroom.com/webapp/contact.html"
+    When I click on the "Send" button
+    And I dismiss the alert
+    Then I am on the "Contact" page
+
